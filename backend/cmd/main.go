70fa91ed34afcf0087b8c38bcdd5338cee4353b7
John Pangalos <john@pangalos.dev>
2021-10-05 22:26:39 +0200

    Get docker working
    Also adding base64 encoding of a service account

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,11 +1,16 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+	"fmt"
+
+	fiber "github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
-	app.Get("/api/list", func(c *fiber.Ctx) {
-		c.SendString("I'm a GET request!")
+	app.Get("/api/list", func(c *fiber.Ctx) error {
+		return c.JSON()
 	})
+	fmt.Println("Running on http://localhost:8080")
 	app.Listen(":8080")
 }
--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,5 @@ module github.com/johnpangalos/squiggles
 go 1.16
 
 require (
-	github.com/gofiber/fiber v1.14.6
-	golang.org/x/sys v0.0.0-20210112080510-489259a85091 // indirect
+	github.com/gofiber/fiber/v2 v2.19.0
 )
