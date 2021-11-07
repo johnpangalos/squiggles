6d97ceec7bcb3fc2a9a05234a7e3502d4f4e002f
John Pangalos <john@pangalos.dev>
2021-11-07 18:40:21 +0100

    follow golang standard project layout

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	app := fiber.New()
 	app.Get("/api/list", func(c *fiber.Ctx) error {
-		return c.JSON()
+		return c.JSON(fiber.Map{"success": true})
 	})
 	fmt.Println("Running on http://localhost:8080")
 	app.Listen(":8080")
