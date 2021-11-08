3132eb525b9917e93616de49dd49d53ccd44caaf
John Pangalos <john@pangalos.dev>
2021-11-08 08:34:54 +0100

    adding dotenv

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	// "os"
+	// "context"
 
 	fiber "github.com/gofiber/fiber/v2"
+
+	"github.com/joho/godotenv"
+	// fb "firebase.google.com/go/v4"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// serviceAccountBase64 := os.Getenv("SERVICE_ACCOUNT")
+	// ctx := context.Background()
+	// fb.NewApp(ctx, )
+
 	app := fiber.New()
 	app.Get("/api/list", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{"success": true})
 	})
-	fmt.Println("Running on http://localhost:8080")
+	log.Printf("Running on http://localhost:8080")
 	app.Listen(":8080")
 }
--- go.mod
+++ go.mod
@@ -3,5 +3,7 @@ module github.com/johnpangalos/squiggles
 go 1.16
 
 require (
+	firebase.google.com/go/v4 v4.6.1
 	github.com/gofiber/fiber/v2 v2.19.0
+	github.com/joho/godotenv v1.4.0
 )
